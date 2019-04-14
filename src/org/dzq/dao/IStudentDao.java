55693de46825e234509703a6ce8d500b14c4b73e
yintianchou5 <33652318+yintianchou5@users.noreply.github.com>
2019-04-14 13:55:14 +0800

    Update IStudentDao.java

--- src/org/dzq/dao/IStudentDao.java
+++ src/org/dzq/dao/IStudentDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.dzq.entity.Student;
 //接口
 //这是我修改的地方
+//第二次修改
 public interface IStudentDao {
 	public boolean updateStudentBySno(int sno,Student student);
 	public boolean deleteStudentBySno(int sno);
